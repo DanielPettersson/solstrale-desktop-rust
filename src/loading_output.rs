4831429addb3d4ff8185ce443ba82e3b4e0c96cf
Daniel Pettersson <daniel.g.pettersson@gmail.com>
2023-06-04 19:07:23 +0200

    Added text to loading output

--- src/loading_output.rs
+++ src/loading_output.rs
@@ -1,4 +1,4 @@
-use eframe::egui::{Color32, Pos2, Rect, Stroke, Ui};
+use eframe::egui::{Align2, Color32, FontId, Pos2, Rect, Stroke, Ui};
 use eframe::{egui, emath, epaint};
 use emath::RectTransform;
 use epaint::Shape;
@@ -17,20 +17,29 @@ pub fn show(ui: &mut Ui) {
         for &mode in &[2, 3, 5] {
             let mode = mode as f64;
             let n = 120;
-            let speed = 1.5;
 
             let points: Vec<Pos2> = (0..=n)
                 .map(|i| {
                     let t = i as f64 / (n as f64);
-                    let amp = (time * speed * mode).sin() / mode;
+                    let amp = (time * mode).sin() / mode;
                     let y = amp * (t * TAU / 2.0 * mode).sin();
                     to_screen * Pos2::new(t as f32, y as f32)
                 })
                 .collect();
 
-            let thickness = 10.0 / mode as f32;
-            shapes.push(Shape::line(points, Stroke::new(thickness, Color32::GRAY)));
+            let thickness = 20.0 / mode as f32;
+            shapes.push(Shape::line(points, Stroke::new(thickness, Color32::from_rgb(50, 50, 50))));
         }
+        ui.fonts(|fonts|
+            shapes.push(Shape::text(
+                fonts,
+                to_screen * Pos2::new(0.5, 0.0),
+                Align2::CENTER_CENTER,
+                "Preparing scene...",
+                FontId::monospace(to_screen.scale().x * 0.04),
+                Color32::WHITE,
+            ))
+        );
 
         ui.painter().extend(shapes);
     });
